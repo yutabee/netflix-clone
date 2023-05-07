ed745cc18fd6cc2ea3b9da1c185278caa2e7cc75
yutabee <bsk87m@gmail.com>
2023-05-07 11:45:39 +0900

    implemented signout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
 
@@ -11,16 +12,19 @@ export async function getServerSideProps(context: NextPageContext) {
         permanent: false,
       },
     };
-    return {
-      props: {},
-    };
   }
+  return {
+    props: {},
+  };
 }
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
       <h1 className="text-4xl text-green-500">Netflix Clone</h1>
+      <p className="text-white">Logged in as : {user?.name}</p>
       <button className="h-10 w-full bg-white" onClick={() => signOut()}>
         Logout
       </button>
