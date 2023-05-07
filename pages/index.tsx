1aee4cdee9e2681bae6c62af519711e69e0796bb
yutabee <bsk87m@gmail.com>
2023-05-07 11:21:25 +0900

    created signout.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
-import { Inter } from "next/font/google";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
 
-const inter = Inter({ subsets: ["latin"] });
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+    return {
+      props: {},
+    };
+  }
+}
 
 export default function Home() {
   return (
     <>
-      <h1>Netflix Clone</h1>
+      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        Logout
+      </button>
     </>
   );
 }
